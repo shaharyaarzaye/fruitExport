31cb649939686556b37df2e6d6aaab90dc864190
Shaharyaar <73778412+shaharyaarzaye@users.noreply.github.com>
2026-01-23 00:27:11 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,7 +3,9 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import EnquiryModal from './components/EnquiryModal';
 import { LanguageProvider } from './context/LanguageContext';
+import { EnquiryProvider } from './context/EnquiryContext';
 
 // Pages
 import Home from './pages/Home';
@@ -11,6 +13,7 @@ import About from './pages/About';
 import Products from './pages/Products';
 import Gallery from './pages/Gallery';
 import Contact from './pages/Contact';
+import ProductDetail from './pages/ProductDetail';
 
 // Scroll to top on route change
 const ScrollToTop = () => {
@@ -24,24 +27,28 @@ const ScrollToTop = () => {
 const App: React.FC = () => {
   return (
     <LanguageProvider>
-      <BrowserRouter>
-        <ScrollToTop />
-        <div className="min-h-screen bg-white selection:bg-yellow-500 selection:text-white flex flex-col">
-          <Navbar />
-          
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/gallery" element={<Gallery />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
-          </main>
+      <EnquiryProvider>
+        <BrowserRouter>
+          <ScrollToTop />
+          <div className="min-h-screen bg-white selection:bg-yellow-500 selection:text-white flex flex-col">
+            <Navbar />
+            
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/products/:id" element={<ProductDetail />} />
+                <Route path="/gallery" element={<Gallery />} />
+                <Route path="/contact" element={<Contact />} />
+              </Routes>
+            </main>
 
-          <Footer />
-        </div>
-      </BrowserRouter>
+            <Footer />
+            <EnquiryModal />
+          </div>
+        </BrowserRouter>
+      </EnquiryProvider>
     </LanguageProvider>
   );
 };
